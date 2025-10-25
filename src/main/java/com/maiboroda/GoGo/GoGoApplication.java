11b39c71594d7023bd954471f28b198ace9bc35d
artur <mauboroda.arturtu@gmail.com>
2025-10-25 10:46:31 +0300

    Create simple Spring project with spring.start -add properties.yml

---
+++ src/main/java/com/maiboroda/GoGo/GoGoApplication.java
@@ -0,0 +1,13 @@
+package com.maiboroda.GoGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGoApplication.class, args);
+	}
+
+}
