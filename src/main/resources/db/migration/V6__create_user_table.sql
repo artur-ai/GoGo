62b23d2097918e4e07db8d55b9c7002560d830a2
DKfedko <dkfedko@yahoo.com>
2026-01-21 14:45:39 -0500

    refactor

--- src/main/resources/db/migration/V6__create_user_table.sql
+++ src/main/resources/db/migration/V6__create_user_table.sql
@@ -6,3 +6,20 @@ CREATE TABLE users (
     middle_name VARCHAR(55),
     date_of_birth DATE NOT NULL
 );
+
+CREATE TABLE reviews (
+    id BIGSERIAL PRIMARY KEY,
+    users_id BIGINT NOT NULL users(id) ON DELETE CASCADE
+
+    first_name VARCHAR(55) NOT NULL,
+    town       VARCHAR(50) NOT NULL,
+    user_age   DATE
+);
+
+CREATE TABLE address (
+    id BIGSERIAL PRIMARY KEY,
+    users_id BIGINT NOT NULL users(id) ON DELETE CASCADE
+
+    countries VARCHAR(50) NOT NULL,
+    town      VARCHAR(50) NOT NULL
+);
--- src/main/resources/db/migration/V8__create_adderss_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE address (
-    id BIGSERIAL PRIMARY KEY,
-    users_id BIGINT NOT NULL users(id) ON DELETE CASCADE
-
-    countries VARCHAR(50) NOT NULL,
-    town      VARCHAR(50) NOT NULL
-);
