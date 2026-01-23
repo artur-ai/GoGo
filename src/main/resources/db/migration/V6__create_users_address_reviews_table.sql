8555e9b2345e86df06d0bfa2e6673bbec53501c4
DKfedko <dkfedko@yahoo.com>
2026-01-23 05:09:35 -0500

    add db migration V6 tables user, addresses, reviews. Create V7-9 for seeds for tests

---
+++ src/main/resources/db/migration/V6__create_users_address_reviews_table.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+
+    first_name  VARCHAR(55) NOT NULL,
+    last_name VARCHAR(55) NOT NULL,
+    middle_name VARCHAR(55),
+    date_of_birth DATE NOT NULL,
+    driver_license_id VARCHAR(15) UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
+    review_text TEXT NOT NULL
+);
+
+CREATE TABLE addresses (
+    id BIGSERIAL PRIMARY KEY,
+
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    country         VARCHAR(50) NOT NULL,
+    town            VARCHAR(50) NOT NULL
+);
