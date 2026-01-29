c5911529fd04bd5087f005b3f1190c0d0e276409
DKfedko <dkfedko@yahoo.com>
2026-01-29 06:04:30 -0500

    add JPA, Hiberne to tabsles to V6

---
+++ src/main/java/com/maiboroda/GoGo/entity/Address.java
@@ -0,0 +1,28 @@
+package com.maiboroda.GoGo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String country;
+
+    @Column(nullable = false, length = 50)
+    private String town;
+}
