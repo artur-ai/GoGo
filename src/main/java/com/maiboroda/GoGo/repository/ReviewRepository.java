ca9f54ab3dc3b53b7cbd8a43d98996fc86f6c040
DKfedko <dkfedko@yahoo.com>
2026-02-09 03:58:10 -0500

    refactor

--- src/main/java/com/maiboroda/GoGo/repository/ReviewRepository.java
+++ src/main/java/com/maiboroda/GoGo/repository/ReviewRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query(value = "SELECT * FROM reviews ORDER BY id DESC LIMIT :count", nativeQuery = true)
+    @Query(value = "SELECT id, user_id, review_text FROM reviews ORDER BY id DESC LIMIT :count", nativeQuery = true)
     List<Review> findLastReviews(@Param("count") int count);
 }
