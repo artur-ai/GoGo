fec825dfc29b70f2b5143f9d3e520e1dcb562fa7
artur <mauboroda.arturtu@gmail.com>
2026-03-09 10:58:22 +0200

    Change api_url in auth.js for AWS

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080/api/v1/auth';
+const API_URL = 'http://gogo-server-env.eba-t8sdmyhd.eu-north-1.elasticbeanstalk.com/api/v1/auth';
 
 const registerForm = document.getElementById('registration-form');
 
